4d9c6f424e091f87ef4a0d045d75cf4f0ac49fc8
tom-does-code <halcyon451@outlook.com>
2022-06-15 23:36:05 +0100

    Add project files.

---
+++ RustPlugin/HelpCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("HelpCommand", "Tom", "0.0.1")]
+    class HelpCommand : RustPlugin
+    {
+        [ChatCommand("help")]
+        void help (BasePlayer player)
+        {
+            SendReply(player, "Welcome to Rustooze.net. This server is still being worked on and is no way near done, join the discord server for more information.");
+        }
+
+        [ConsoleCommand("ConsoleTest")]
+        
+        void consoletest(ConsoleSystem.Arg args)
+        {
+            Puts("Hello");
+        }
+    }
+}
